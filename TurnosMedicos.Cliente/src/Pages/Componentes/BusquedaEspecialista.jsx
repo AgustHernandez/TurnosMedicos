097f30f44a5337d0c91d0e8ef8557908e34379c3
AgustHernandez <hagustinadaniela@gmail.com>
2024-01-22 09:09:42 -0300

    Select funcionando con datos de la API

--- TurnosMedicos.Cliente/src/Pages/Componentes/BusquedaEspecialista.jsx
+++ TurnosMedicos.Cliente/src/Pages/Componentes/BusquedaEspecialista.jsx
@@ -1,70 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import '../../App.css';
-import { Autocomplete, Container, TextField } from '@mui/material';
+import { Container } from '@mui/material';
 import Calendario from './Calendario';
 import { Select } from 'antd';
 
-
-const onChange = (value) => {
-    console.log(`selected ${value}`);
-};
-const onSearch = (value) => {
-    console.log('search:', value);
-};
-
-// Filter `option.label` match the user type `input`
-const filterOption = (input, option) =>
-    (option.nombre + option.apellido).toLowerCase().includes(input.toLowerCase());
-
 function BusquedaEspecialista() {
+  const url = "http://localhost:8080/api/especialistas";
+  const [data, setData] = useState([]);
 
-    const url = "http://localhost:8080/api/especialistas";
-    const [data, setData] = useState([]);
-    
-    const info = [{value:'45147',label:'Juan Pablo Martinez'},{value:'78415', label:'Agustina Daniela Hernandez'}]
+  const filterOption = (input, option) =>
+    (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
 
-    const onChange = (value) => {
-        console.log(`Selected: ${value}`);
-    };
-
-    const filterOption = (input, option) =>
-  (option?.label ?? '').toLowerCase().includes(input.toLowerCase());
-  
     const fetchInfo = () => {
         fetch(url)
-        .then((res) => res.json())
-        .then((s) => setData(info))
-    }
+          .then((res) => res.json())
+          .then((s) => {
+            setData(s.map(function (elemento) {
+              return {
+                value: elemento.legajo,
+                label: elemento.apellido + ', ' + elemento.nombre
+              };
+            }))
+          })
+      }
 
-    useEffect(() => {
-        fetchInfo();
-    }, []);
+  useEffect(() => {
+      fetchInfo();
+  }, [data]);
 
-    console.log("Data "+ JSON.stringify(data))
-    
-    return(
-        <Container maxWidth="lg" sx={{marginTop: 0}}>
-            <Container>
-                <Select
-                showSearch
-                placeholder="placeholder"
-                optionFilterProp="children"
-                style={{ width: 300, height:50 }}
-                onChange={onChange}
-                onSearch={onSearch}
-                filterOption={filterOption}
-                options={data}
-            />
-            </Container>
-            <Container>
-                <Calendario/>
-            </Container>
-        </Container>
-    )
+  return (
+    <Container maxWidth="lg" sx={{ marginTop: 0 }}>
+      <Container>
+        <Select
+          showSearch
+          placeholder="Seleccione un especialista"
+          optionFilterProp="children"
+          style={{ width: 300, height: 50 }}
+          filterOption={filterOption}
+          options={data}
+        />
+      </Container>
+      <Container>
+        <Calendario />
+      </Container>
+    </Container>
+  )
 }
 
-export default BusquedaEspecialista
-
+export default BusquedaEspecialista;
 
 /*Autocomplete
                 inputValue={inputValue}
