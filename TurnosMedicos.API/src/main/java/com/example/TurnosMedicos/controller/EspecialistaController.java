ec51d746de1849d2bdc912b26bd7d79320a2fd65
AgustHernandez <hagustinadaniela@gmail.com>
2024-01-26 13:30:29 -0300

    Notificacion via Email

--- TurnosMedicos.API/src/main/java/com/example/TurnosMedicos/controller/EspecialistaController.java
+++ TurnosMedicos.API/src/main/java/com/example/TurnosMedicos/controller/EspecialistaController.java
@@ -1,16 +1,19 @@
 package com.example.TurnosMedicos.controller;
 
 import com.example.TurnosMedicos.DTO.Especialista.EspecialistaDTO;
+import com.example.TurnosMedicos.DTO.Turno.InfoTurnoDTO;
 import com.example.TurnosMedicos.DTO.Turno.TurnoDTO;
 import com.example.TurnosMedicos.exceptions.DuplicatedElementException;
 import com.example.TurnosMedicos.exceptions.ElementAlreadyExistsException;
 import com.example.TurnosMedicos.exceptions.ResourceNotFoundException;
 import com.example.TurnosMedicos.model.Especialista;
 import com.example.TurnosMedicos.model.EspecialistaQuery;
+import com.example.TurnosMedicos.services.JwtTokenService;
 import com.example.TurnosMedicos.services.interfaces.IEspecialistaServ;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,10 +27,12 @@ import java.util.List;
 public class EspecialistaController {
     private static final Logger logger = Logger.getLogger(EspecialistaController.class);
     private final IEspecialistaServ EspecialistaService;
+    private final JwtTokenService JwtService;
 
     @Autowired
-    public EspecialistaController(IEspecialistaServ especialistaService) {
+    public EspecialistaController(IEspecialistaServ especialistaService, JwtTokenService jwtService) {
         this.EspecialistaService = especialistaService;
+        this.JwtService = jwtService;
     }
 
     @GetMapping("/especialistas")
@@ -48,15 +53,27 @@ public class EspecialistaController {
             @PathVariable String matricula,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate fecha) {
         logger.info("GET /especialistas/{}/turnos?fecha={}");
+
         List<TurnoDTO> result = EspecialistaService.obtenerTurnosPorMatriculaYFecha(matricula, fecha);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping("/turnos/{codTurno}")
-    public ResponseEntity<Boolean> guardarTurnoPorCodigo(
-            @PathVariable String codTurno) throws DuplicatedElementException, ResourceNotFoundException {
+    @GetMapping("/especialistas/{matricula}/turnos/{codTurno}")
+    public ResponseEntity<InfoTurnoDTO> obtenerTurnoPorCodigo(
+            @PathVariable String matricula,@PathVariable String codTurno) throws DuplicatedElementException, ResourceNotFoundException {
         logger.info("GET /turnos/{}");
-        Boolean result = EspecialistaService.guardarTurnoPorCodigo(codTurno);
+        InfoTurnoDTO result = EspecialistaService.obtenerTurnoPorCodigo(matricula,codTurno);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("/especialistas/{matricula}/turnos/{codTurno}")
+    public ResponseEntity<Boolean> guardarTurnoPorCodigo(
+            @PathVariable String matricula,
+            @PathVariable String codTurno,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization) throws DuplicatedElementException, ResourceNotFoundException {
+        logger.info("POST /turnos/{}");
+        String username = JwtService.extractUsername(authorization.substring(7));
+        Boolean result = EspecialistaService.guardarTurnoPorCodigo(codTurno,matricula,username);
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- TurnosMedicos.API/src/main/java/com/example/TurnosMedicos/model/AppUser.java
+++ TurnosMedicos.API/src/main/java/com/example/TurnosMedicos/model/AppUser.java
@@ -57,4 +57,20 @@ public class AppUser {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
